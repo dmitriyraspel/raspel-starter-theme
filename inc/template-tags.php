ad905edf28109d5f78ee236218c5150ab1400ed9
dmitriyraspel <dmitriyraspel@yandex.ru>
2020-09-16 14:17:55 +0200

    posted on add svg

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'raspellab_posted_on' ) ) :
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 		}
-
+				
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
@@ -25,16 +25,16 @@ if ( ! function_exists( 'raspellab_posted_on' ) ) :
 			esc_html( get_the_modified_date() )
 		);
 
-		$posted_on = sprintf(
-			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'raspellab' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		printf(
+			'<span class="posted-on">%1$s<a href="%2$s" rel="bookmark">%3$s</a></span>',
+			raspellab_get_icon_svg( 'calendar', 16 ),
+			esc_url( get_permalink() ),
+			$time_string
 		);
-
-		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 	}
 endif;
+
 
 if ( ! function_exists( 'raspellab_posted_by' ) ) :
 	/**
