21258d9502499651dddd222151dc94df8560dfa2
dmitriyraspel <dmitriyraspel@yandex.ru>
2020-10-01 00:38:32 +0200

    add customizer title&description

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -8,25 +8,35 @@
 ?>
 <div class="site-branding">
 
-	<?php if ( has_custom_logo() ) : ?>
-		<div class="site-logo"><?php the_custom_logo(); ?></div>
-	<?php else : 
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php 
-				endif;
-			endif; ?>
+<?php
+	/* Show site logo ---------------------------------------------------- */
+ if ( has_custom_logo() ) : ?>
+	<div class="site-logo"><?php the_custom_logo(); ?></div>
+<?php endif; ?>
 
-	<?php
-	$description = get_bloginfo( 'description', 'display' );
-	if ( $description || is_customize_preview() ) :
-	?>
-			<p class="site-description">
-				<?php echo $description; ?>
-			</p>
+<?php
+	/* Show site title ---------------------------------------------------- */
+	$blog_info = get_bloginfo( 'name' );
+	$site_logo_and_title = get_theme_mod ('site_logo_and_title');
+
+	if ( ! empty( $blog_info ) && $site_logo_and_title ) : ?>
+		<?php if ( is_front_page() && is_home() ) : ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php else : ?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<?php endif;
+	endif; ?>
+
+
+<?php
+	/* Show site description ---------------------------------------------------- */
+	$description = get_bloginfo( 'description');
+	$site_title_and_description = get_theme_mod ('site_title_and_description');
+
+	if ( $description && $site_title_and_description ) : ?>
+		<p class="site-description">
+			<?php echo $description; ?>
+		</p>
 	<?php endif; ?>
+
 </div><!-- .site-branding -->
