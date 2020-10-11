c9319f637ecbd85eeba03c6cf913fdafed039377
dmitriyraspel <dmitriyraspel@yandex.ru>
2020-10-11 12:34:37 +0200

    add assets-style.css> gitignore

--- header.php
+++ header.php
@@ -55,7 +55,13 @@
 						?>
 					</nav><!-- #site-navigation -->
 
-					
+					<!-- <nav class="social-navigation reset-list-style" role="navigation" aria-label="Social Links Menu">
+						<ul id="menu-soczialnoe-menyu" class="menu">
+							<li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7"><a href="#"><span class="screen-reader-text">телега</span><svg class="svg-icon" width="26" height="26" aria-hidden="true" role="img" focusable="false" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+										<path d="M15.364 16.718l1.688-7.955c0.149-0.7-0.253-0.976-0.712-0.804l-9.918 3.823c-0.677 0.264-0.666 0.643-0.115 0.815l2.537 0.792 5.889-3.708c0.275-0.184 0.528-0.080 0.321 0.103l-4.764 4.305-0.184 2.617c0.264 0 0.379-0.115 0.517-0.253l1.24-1.194 2.571 1.894c0.471 0.264 0.804 0.126 0.93-0.436zM22.286 12c0 5.682-4.603 10.286-10.286 10.286s-10.286-4.603-10.286-10.286 4.603-10.286 10.286-10.286 10.286 4.603 10.286 10.286z"></path>
+									</svg></a></li>
+						</ul>
+					</nav> -->
 				</div><!-- /.header-navigation-wrapper -->
 
 			</div><!-- /.header__inner -->
