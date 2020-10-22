a0b8f637313f902ea837bebdf0da55ce7b66b78e
dmitriyraspel <dmitriyraspel@yandex.ru>
2020-10-22 13:09:24 +0200

    change show title (h1 or p+link)

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,11 +1,19 @@
 <?php
+
 /**
  * Displays header site branding
  *
- * @package Raspel
- * @since 1.0.0
+ * @package my_first_theme_for-block
+ *
  */
+
+$blog_info    		= get_bloginfo('name');
+$description 		= get_bloginfo('description');
+$show_title   		= get_theme_mod('site_logo_and_title');
+$show_description	= get_theme_mod('site_title_and_description');
+
 ?>
+
 <div class="site-branding">
 
 	<?php
@@ -14,32 +22,36 @@
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
 
+
+	<?php if ( ( ! empty( $blog_info ) && $show_title ) || ( $description && $show_description ) ) : ?>
 	<div class="site-branding__inner">
-
 		<?php
-		/* Show site title ---------------------------------------------------- */
-		$blog_info = get_bloginfo('name');
-		$site_logo_and_title = get_theme_mod('site_logo_and_title');
-
-		if (!empty($blog_info) && $site_logo_and_title) : ?>
-			<?php if (is_front_page() && is_home()) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+		/* 
+		*	Show site title
+		*/
+		if ( ! empty( $blog_info ) && $show_title ) : ?>
+			<?php if ( is_front_page() && !is_paged() ) : ?>
+				<h1 class="site-title"><?php bloginfo('name'); ?></h1>
+			<?php elseif ( is_front_page() || is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-		<?php endif;
-		endif; ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url('/') ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php endif; ?>
+		<?php endif; ?>
 
 
 		<?php
-		/* Show site description ---------------------------------------------------- */
-		$description = get_bloginfo('description');
-		$site_title_and_description = get_theme_mod('site_title_and_description');
-
-		if ($description && $site_title_and_description) : ?>
+		/* 
+		*	Show site description
+		*/
+		if ( $description && $show_description ) : ?>
 			<p class="site-description">
 				<?php echo $description; ?>
 			</p>
 		<?php endif; ?>
 	</div><!-- /.site-branding__inner -->
+	<?php endif; ?>
+
+	
 
 </div><!-- .site-branding -->
