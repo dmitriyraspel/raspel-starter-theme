b990b32a7c76268f7c7957932a794d0a97bed810
dmitriyraspel <dmitriyraspel@yandex.ru>
2020-10-22 14:54:47 +0200

    del entry-header&post_thumbnail from front page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,11 +10,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<?php raspellab_post_thumbnail(); ?>
+	<?php if ( ! is_front_page() ) : ?>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+		
+		<?php raspellab_post_thumbnail(); ?>
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php
